e57546a53ec2fe3acb0a87c5d51b4d4ec057e7a0
Just Jase <tayjiasheng@Jias-MacBook-Pro.local>
2026-02-18 13:30:28 +0800

    add completed auth middlewares to codebase

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,36 @@
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import jwt from "jsonwebtoken";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+  const token =
+    req.cookies?.accessToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
+  // MAKE SURE the 'accessToken' key matches what is in the response json! (when u test the login route, look at the response json, the key is 'accessToken')
+  // or you can fetch the accessToken via the header - but make sure to replace 'Bearer' with nothing so you end up with the access token ONLY
+
+  if (!token) {
+    throw new ApiError(401, "Unauthorized request"); // abort the request
+  }
+
+  // safely decode aka validate the access token (how does the decoding work? is this the signing process..?)
+  try {
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET); // this is an object
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
+    );
+
+    // how do u know _id is there - check user.models.js, we have provided the user._id property in the payload when we issue an accessToken to the user. (also how come the function only has the payload and secret.. where's the header part in the function when we issue the jwt???)
+    // for the select() refer to your auth.controller.js to remove the same fields that you don't want
+
+    if (!user) {
+      throw new ApiError(401, "Invalid access token");
+    }
+
+    req.user = user; // add an additional property -> user.
+    next(); // proceed to the next middleware or the next part of the controller itself
+  } catch (error) {
+    throw new ApiError(401, "Invalid access token.");
+  }
+});
