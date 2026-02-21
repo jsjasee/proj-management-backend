e1e5b349df04d5d8b256ccb55bef1717684fb700
Just Jase <tayjiasheng@Jias-MacBook-Pro.local>
2026-02-21 14:58:34 +0800

    add completed project routes to codebase

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import healthcheckRouter from "./routes/healthcheck.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
+import projectRouter from "./routes/project.routes.js";
 
 const app = express();
 
@@ -26,6 +27,7 @@ app.use(
 // import the routes (must use USE NOT GET!) - check Proj resource docs (PRD) for the routes.
 app.use("/api/v1/healthcheck", healthcheckRouter); // if got extra stuff like you want to hit "/api/v1/healthcheck/instagram, you DON'T need to change this, you just create another '/instagram' in healthcheckRouter function"
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/projects", projectRouter);
 
 // below means use the app, then the GET method (there's only get, post, put/patch & delete), then GET from which url (this is the ROUTES), followed by callback (request & response) -> data sent from client is request, data sent from server is response.
 app.get("/", (req, res) => {
---
+++ src/routes/project.routes.js
@@ -0,0 +1,61 @@
+// the boilerplate for routes is the same
+import { Router } from "express";
+import {
+  addMembersToProject,
+  createProject,
+  getProjects,
+  getProjectById,
+  updateMemberRole,
+  updateProject,
+  getProjectMembers,
+  deleteMember,
+  deleteProject,
+} from "../controllers/project.controller.js";
+import { validate } from "../middlewares/validator.middleware.js";
+import {
+  createProjectValidator,
+  addMemberToProjectValidator,
+} from "../validators/index.js";
+import {
+  verifyJWT,
+  validateProjectPermission,
+} from "../middlewares/auth.middleware.js";
+import { AvailableUserRole, UserRolesEnum } from "../utils/constants.js";
+
+const router = Router(); // there's no need to write 'new' keyword for Router() even though you can, express.js will auto create and return a new function when Router() is written. don't need to use 'new' to set up the context for 'this'
+
+// .use() is like a middleware, we pass in verifyJWT means all the routes in this file will have a verifyJWT AS THE FIRST middleware
+router.use(verifyJWT);
+
+router
+  .route("/")
+  .get(getProjects)
+  .post(createProjectValidator(), validate, createProject); // validate will access the errors in the req.body
+
+router
+  .route("/:projectId") // make sure this projectId matches whatever you are catching in the req.params
+  .get(validateProjectPermission(AvailableUserRole), validate, getProjectById) // handle the GET request. we pass in the ENTIRE AvailableUserRole array which means EVERYONE can perform access route
+  .put(
+    validateProjectPermission([UserRolesEnum.ADMIN]),
+    createProjectValidator(),
+    validate,
+    updateProject,
+  )
+  .delete(validateProjectPermission([UserRolesEnum.ADMIN]), deleteProject);
+
+router
+  .route("/:projectId/members") // if no ":" express does NOT treat it as a param, : means it gets treated as a param, members is not treated like a param here
+  .get(getProjectMembers)
+  .post(
+    validateProjectPermission([UserRolesEnum.ADMIN]),
+    addMemberToProjectValidator(),
+    validate,
+    addMembersToProject,
+  );
+
+router
+  .route("/:projectId/members/:userId")
+  .put(validateProjectPermission([UserRolesEnum.ADMIN]), updateMemberRole)
+  .delete(validateProjectPermission([UserRolesEnum.ADMIN]), deleteMember);
+
+export default router;
